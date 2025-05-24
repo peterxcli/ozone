7e924ba6c62da3673bbecde142e38d2fcb6824ed
peterxcli <peterxcli@gmail.com>
2025-05-24 15:15:53 +0800

    Add CompoundKeyRangeStats class for managing key range statistics and enhance KeyRange constructor to support byte array inputs. Update OBSTableCompactor to utilize new CompoundKeyRangeStats for improved compaction logic.

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/compaction/OBSTableCompactor.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/compaction/OBSTableCompactor.java
@@ -17,15 +17,28 @@
 
 package org.apache.hadoop.ozone.om.compaction;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.hadoop.hdds.StringUtils;
 import org.apache.hadoop.hdds.utils.db.KeyRange;
+import org.apache.hadoop.hdds.utils.db.RDBStore;
+import org.apache.hadoop.hdds.utils.db.RocksDatabaseException;
 import org.apache.hadoop.hdds.utils.db.cache.CacheKey;
 import org.apache.hadoop.hdds.utils.db.cache.CacheValue;
 import org.apache.hadoop.ozone.om.helpers.OmBucketInfo;
+import org.rocksdb.LiveFileMetaData;
+import org.rocksdb.TableProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.api.Distribution.Range;
 
 /**
  * Compactor for OBS and legacy layout that compacts based on bucket ranges.
@@ -116,6 +129,56 @@ public class OBSTableCompactor extends AbstractCompactor {
       bucketEndKey = getKeyPrefixUpperBound(bucketStartKey);
     }
     return new KeyRange(bucketStartKey, bucketEndKey);
+  }
+
+  private List<KeyRange> prepareRanges() throws IOException {
+    // dd a function to prepare ranges first, start from `nextKey` or
+    // `currentBucketBoundary`.
+    // This can be done by getting the live file metadata list, then use
+    // `getPropsOfTableInRange` to retrieve SSTs in range `[nextKey,
+    // currentBucketBoundary]` or `[currentBucket, currentBucketBoundry]`,
+    // and if there are too many entries in these ranges, we need to further spilt
+    // them into smaller one, them save spilt point in `nextKey` for next round to
+    // use.
+    // In `collectRangesNeedingCompaction`, it call the previous
+    List<KeyRange> ranges;
+    long accumulatedEntries = 0;
+
+    while (accumulatedEntries <= getMaxEntriesSum()) {
+      if (nextKey != null) {
+        // handle pagination
+      }
+    }
+    String startKey, endKey;
+
+  }
+
+  private CompoundKeyRangeStats getCompoundKeyRangeStatsFromRange(KeyRange range) throws IOException {
+    List<Pair<KeyRange, KeyRangeStats>> keyRangeStatsList = new ArrayList<>();
+    List<LiveFileMetaData> liveFileMetaDataList = ((RDBStore)getDBStore()).getDb().getLiveFilesMetaData();
+    Map<String, LiveFileMetaData> fileMap = new HashMap<>();
+    // https://github.com/facebook/rocksdb/blob/09cd25f76305f2110131f51068656ab392dc2bf5/db/version_set.cc#L1744-L1768
+    // https://github.com/facebook/rocksdb/blob/a00391c72996a5dbdd93a621dbc53719c13b05c4/file/filename.cc#L140-L150
+    for (LiveFileMetaData metaData : liveFileMetaDataList) {
+      fileMap.put(metaData.fileName(), metaData);
+    }
+
+    Map<String, TableProperties> propsMap = getDBStore().getPropertiesOfTableInRange(getTableName(),
+        Collections.singletonList(range));
+
+    for (Map.Entry<String, TableProperties> entry : propsMap.entrySet()) {
+      String fullPath = entry.getKey();
+      String fileName = fullPath.substring(fullPath.lastIndexOf('/') + 1);
+      LiveFileMetaData meta = fileMap.get(fileName);
+      if (meta == null) {
+        LOG.warn("LiveFileMetaData not found for file {}", fullPath);
+        continue;
+      }
+      KeyRange keyRange = new KeyRange(meta.smallestKey(), meta.largestKey());
+      KeyRangeStats stats = KeyRangeStats.fromTableProperties(entry.getValue());
+      keyRangeStatsList.add(Pair.of(keyRange, stats));
+    }
+    return new CompoundKeyRangeStats(keyRangeStatsList);
   }
 
   // private String findSplitKey(KeyRange range) {
