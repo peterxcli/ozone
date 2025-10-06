5d0bca62b4153b94bb6608a7de07d7195768fbf9
chungen0126 <wfps1210@gmail.com>
2025-10-06 21:48:04 +0800

    create admin gateway daemon

---
+++ hadoop-ozone/admin-gateway/src/main/java/org/apache/ozone/admin/Gateway.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ozone.admin;
+
+import java.util.concurrent.Callable;
+import org.apache.hadoop.hdds.cli.GenericCli;
+import org.apache.hadoop.hdds.cli.HddsVersionProvider;
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.ozone.util.OzoneNetUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine.Command;
+
+/**
+ * This class is used to start/stop admin rest server.
+ */
+@Command(name = "ozone ag",
+    hidden = true, description = "admin rest server.",
+    versionProvider = HddsVersionProvider.class,
+    mixinStandardHelpOptions = true)
+public class Gateway extends GenericCli implements Callable<Void> {
+
+  private static Logger LOG = LoggerFactory.getLogger(Gateway.class);
+
+  public static void main(String[] args) throws Exception {
+    OzoneNetUtils.disableJvmNetworkAddressCacheIfRequired(
+        new OzoneConfiguration());
+    new Gateway().run(args);
+  }
+
+
+  @Override
+  public Void call() throws Exception {
+    start();
+    return null;
+  }
+
+  public void start() {
+    LOG.info("Starting Ozone admin gateway");
+  }
+
+  public void stop() {
+    LOG.info("Stoping Ozone admin gateway");
+  }
+}
